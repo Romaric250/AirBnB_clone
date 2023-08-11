2c72d66ae468dcd9fadf6c99d2a19fd1d7311474
Romaric250 <lonfonyuyromeo@gmail.com>
2023-08-11 00:32:29 -1100

    created the amenity cclass

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this module contains the amenity class"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """this is the amenity class"""
+
+    name = ""
