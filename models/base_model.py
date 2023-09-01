8a3ec6844d7f971133621c85ad192722f6faa323
Romaric250 <lonfonyuyromeo@gmail.com>
2023-09-01 00:01:22 -1100

    some task

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -31,7 +32,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
+            storage.new(self)
     def __str__(self):
         """Returns official string representation"""
 
@@ -42,6 +43,7 @@ class BaseModel:
         """updates the public instance attribute updated_at"""
 
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
